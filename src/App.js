aabaec576a6f5912d71daa99437cd5cadb09f32c
Aditya-Khoje <adityakhoje1@gmail.com>
2023-09-12 19:44:47 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Textform from "./Components/Textform";
+import About from "./Components/About";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About" />
       <Textform heading="Enter Text To Analyze" />
+      {/* <About /> */}
       <Footer />
     </>
   );
